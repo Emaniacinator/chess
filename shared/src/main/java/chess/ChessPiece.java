7a838e0ace74c1a42ee3c96814da3f045c6aa6cf
Emaniacinator <TeamVinton5@gmail.com>
2025-01-24 20:47:15 -0700

    Built the Queen and Rook movement. Refactored old code to reduce bugs. Began implementing movement in main ChessPiece class

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.ChessPieces.*;
+
 import java.util.Collection;
 
 /**
@@ -12,11 +14,21 @@ public class ChessPiece {
 
     protected ChessGame.TeamColor pieceColor;
     protected ChessPiece.PieceType type;
+    protected boolean hasMoved;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type, boolean hasMoved) {
+
+        this.pieceColor = pieceColor;
+        this.type = type;
+        this.hasMoved = hasMoved;
+
+    }
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
 
         this.pieceColor = pieceColor;
         this.type = type;
+        this.hasMoved = false;
 
     }
 
@@ -54,6 +66,8 @@ public class ChessPiece {
     }
 
 
+    // In the coding test, this might all just be better implemented as a set of helper functions instead of as
+    // a bunch of individual inherited classes
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -62,7 +76,39 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+
+        ChessPiece grabbedPiece = board.getPiece(myPosition);
+        PieceType currentPieceType = grabbedPiece.getPieceType();
+
+        switch (currentPieceType) {
+
+            case KING:
+                King castToKing = new King(grabbedPiece.pieceColor, currentPieceType, myPosition, grabbedPiece.hasMoved);
+                return castToKing.pieceMoves(board, myPosition);
+
+            case QUEEN:
+                Queen castToQueen = new Queen(grabbedPiece.pieceColor, currentPieceType, myPosition);
+                return castToQueen.pieceMoves(board, myPosition);
+
+            case BISHOP:
+                Bishop castToBishop = new Bishop(grabbedPiece.pieceColor, currentPieceType, myPosition);
+                return castToBishop.pieceMoves(board, myPosition);
+
+            case KNIGHT:
+                Knight castToKnight = new Knight(grabbedPiece.pieceColor, currentPieceType, myPosition);
+                return castToKnight.pieceMoves(board, myPosition);
+
+            case ROOK:
+                Rook castToRook = new Rook(grabbedPiece.pieceColor, currentPieceType, myPosition, grabbedPiece.hasMoved);
+                return castToRook.pieceMoves(board, myPosition);
+
+            case PAWN:
+                Pawn castToPawn = new Pawn(grabbedPiece.pieceColor, currentPieceType, myPosition, grabbedPiece.hasMoved);
+                return castToPawn.pieceMoves(board, myPosition);
+        }
+
+        return null;
+
     }
 
 
