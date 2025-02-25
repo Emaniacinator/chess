db0f75403c4b17109be506811962f3848d9fe52b
Emaniacinator <TeamVinton5@gmail.com>
2025-02-24 20:04:26 -0700

    Made a lot of progress on the DataAccess get and add UserData methods. Also touched up some other code

--- server/src/main/java/dataaccess/GeneralDataAccess.java
+++ server/src/main/java/dataaccess/GeneralDataAccess.java
@@ -3,17 +3,44 @@ package dataaccess;
 import chess.model.AuthData;
 import chess.model.GameData;
 import chess.model.UserData;
-import dataaccess.DataAccessFramework;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class GeneralDataAccess implements DataAccessFramework{
 
+
+    final private HashMap<Integer, UserData> userDataMap = new HashMap<>();
+    final private HashMap<Integer, AuthData> authDataMap = new HashMap<>();
+    final private HashMap<Integer, GameData> gameDataMap = new HashMap<>();
+
+
     public UserData addUserData(UserData newUserData) {
-        return null;
+
+        // Should this return AuthData instead? Like, make and then return it? Or should that be done by the Service
+
+        userDataMap.put(newUserData.hashCode(), newUserData);
+
+        return newUserData;
+
     }
 
 
     public UserData getUserData(String username) {
+
+        for (Map.Entry<Integer, UserData> currentData : userDataMap.entrySet()){
+
+            UserData checkedData = currentData.getValue();
+            if (Objects.equals(checkedData.username(), username)){
+
+                return checkedData;
+
+            }
+
+        }
+
         return null;
+
     }
 
 
