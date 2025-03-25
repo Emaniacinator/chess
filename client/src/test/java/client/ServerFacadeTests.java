3d5981007fb717005f179a4ed7ba9ff3d091ffbc
Emaniacinator <TeamVinton5@gmail.com>
2025-03-25 12:47:10 -0600

    Fixed duplicate server run statement and resulting port issues

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -28,9 +28,7 @@ public class ServerFacadeTests {
 
         server = new Server();
 
-        var port = server.run(0);;
-
-        server.run(port);
+        var port = server.run(0);
 
         System.out.println("Started test HTTP server on " + port);
 
